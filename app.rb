b2b1022e9d78ff1c1f771538a21c87f8e26c4f46
Miles Baccari <Miles@Miless-MacBook-Air-2.local>
2016-06-02 15:24:43 -0400

    Adding all files

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "sinatraDatabase.sqlite3" }
+
+require './models.rb'
+
+get '/' do 
+	erb :index
+end 
+
+get '/sign-in' do 
+	erb :sign_in
+end 
+
+post '/sign-in-process' do 
+	@username = params[:username]
+	@password = params[:password]
+	redirect('/welcome?username=#{@username}')
+end 
+
+get '/welcome' do 
+	@username = params[:username]
+	erb :welcome
+end 
+
